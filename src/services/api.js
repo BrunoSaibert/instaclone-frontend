596b4924606335b3ccd0a15e67f37f300fbda8ff
BrunoSaibert <brunosaibert@gmail.com>
2019-06-20 13:10:33 -0300

    Adiciona variável ambiente pra URL da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-// require('dotenv').config();
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export default api;
